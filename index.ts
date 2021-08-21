4a57c62c9d33b365292c382051b57c428cf9e2ec
lahlim <lassi_mustonen@hotmail.com>
2021-08-21 08:57:21 +0300

    Eslint installed

--- index.ts
+++ index.ts
@@ -1,14 +1,12 @@
-const ical = require('node-ical');
+import { getEventDuration } from './utils';
 require('dotenv').config();
-import {getEventDuration} from "./utils"
-
+const ical = require('node-ical');
 
 
 const getData = async () => {
-  const data = await ical.async.fromURL(process.env.CAL_ADDRESS);
-  console.log(data);
+    const data = await ical.async.fromURL(process.env.CAL_ADDRESS);
+    console.log(data);
 };
 getData();
-
 
 console.log(getEventDuration("2021-08-19T05:30:00.000Z { tz: 'Etc/UTC' }", "2021-08-19T06:00:00.000Z { tz: 'Etc/UTC' }"));
--- utils.ts
+++ utils.ts
@@ -1,22 +1,17 @@
 // Gets start and end time of event and returns duration in minutes
 const getEventDuration = (start:string, end:string) => {
-    const [startTime] = start.split(" ");
-    const [endTime] = end.split(" ");
-    const startDate = new Date(startTime);
-    const endDate   = new Date(endTime);
-    const diff = Math.abs(startDate.valueOf() - endDate.valueOf());
-    const minutes = Math.floor((diff/1000)/60);
-    console.log(minutes);
-    return minutes;
-}
+  const [startTime] = start.split(' ');
+  const [endTime] = end.split(' ');
+  const startDate = new Date(startTime);
+  const endDate = new Date(endTime);
+  const diff = Math.abs(startDate.valueOf() - endDate.valueOf());
+  const minutes = Math.floor((diff / 1000) / 60);
+  console.log(minutes);
+  return minutes;
+};
 
+const dataToArray = () => '';
 
-const dataToArray = () => {
-    return "";
-}
+const addDuration = () => '';
 
-const addDuration = () => {
-    return "";
-}
-
-export {getEventDuration, addDuration, dataToArray};
+export { getEventDuration, addDuration, dataToArray };
